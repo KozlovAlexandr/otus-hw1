9228f6eb621663732c1fff429e22fda24ea9f7cd
Alexander Kozlov <kostebo98@mail.ru>
2020-07-27 22:36:03 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout << "build " << version()  << endl;
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
